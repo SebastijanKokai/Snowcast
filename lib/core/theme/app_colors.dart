85b008456cb40bf4e75e03379ceac3513058c707
Sebastijan Kokai <sebastijan.kokai@outlook.com>
2025-05-25 21:19:17 +0200

    Put colors into a separate AppColors file

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Weather background colors
+  static const thunderBackground = Color(0xFF2C3E50);
+  static const snowBackground = Color(0xFFE0F7FA);
+  static const rainBackground = Color(0xFFE3F2FD);
+  static const fogBackground = Color(0xFFECEFF1);
+  static const cloudyBackground = Color(0xFFF5F5F5);
+  static const defaultBackground = Color(0xFFE8F5E9);
+
+  // Weather text colors
+  static const thunderText = Colors.white;
+  static const snowText = Color(0xFF01579B);
+  static const rainText = Color(0xFF0D47A1);
+  static const fogText = Color(0xFF37474F);
+  static const cloudyText = Color(0xFF424242);
+  static const defaultText = Color(0xFF1B5E20);
+}
